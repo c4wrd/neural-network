cb4b8a9eed019299b1a8998f63a75500dc2dba63
Cory Forward <cory@linux.com>
2017-10-14 12:20:30 -0600

    ...

--- experiment.py
+++ experiment.py
@@ -5,9 +5,25 @@ from kfold import *
 import sys
 import csv
 
+class CachedWriter:
+    
+    def __init__(self, writer):
+        self.writer = writer
+        self.queue = []
+    
+    def write_row(self, row):
+        self.queue.append(row)
+        if len(self.queue) == 100:
+            while len(self.queue) > 0:
+                self.writer.writerow(self.queue.pop())
+
+    def flush(self):
+        while len(self.queue) > 0:
+                self.writer.writerow(self.queue.pop())
+
 num_epochs = int(sys.argv[1])
 dataset = Datasets.random_rosenbrock(2, points=1000)
-network = MLFFNetwork(2, 1, 15, output_transfer="linear")
+network = MLFFNetwork(2, 1, 10, output_transfer="linear")
 trainer = MLPNetworkTrainer(network)
 k = KFoldCrossValidation(dataset, 5)
 training_set = k.get_training_set(0)
@@ -16,16 +32,22 @@ validation_set = k.get_validation_set(0)
 results_file = open("results.csv", "w+")
 writer = csv.writer(results_file)
 writer.writerow(["epoch", "mse", "validation_mse"])
+writer = CachedWriter(writer)
 
-prev_valid_err = None
-for [epoch, sum_error] in trainer.train_linear_regression(training_set, num_epochs):
-    mse = trainer.mean_squared_error(validation_set)
-    writer.writerow([str(epoch), str(sum_error), str(mse)])
-    if epoch % 10 == 0:
-        print("epoch %d, sum_error=%.3f" % (epoch, sum_error))
-        if prev_valid_err is None:
-            prev_valid_err = mse
-            print("validation mse: %.3f" % mse)
-        else:
-            print("validation mse: %.3f, difference: %.3f" % (mse, prev_valid_err - mse))
-            prev_valid_err = mse
+def run():
+    prev_err = None
+    for [epoch, sum_error] in trainer.train_linear_regression(training_set, num_epochs):
+        writer.write_row([str(epoch), str(sum_error)])
+        if epoch % 10 == 0:
+            if prev_err is None:
+                prev_err = sum_error
+                print("epoch %d, sum_error=%.3f" % (epoch, sum_error))
+            else:
+                diff = prev_err - sum_error
+                print("epoch %d, sum_error=%.3f, diff: %.3f" % (epoch, sum_error, diff))
+                prev_err = sum_error
+
+try:
+    run()
+except:
+    writer.flush()
