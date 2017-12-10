b80f652c2673c965e6e37f584ae1ef59852b39bd
Cory Forward <cory@linux.com>
2017-12-10 15:48:46 -0700

    Added trials to KMeans

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -3,7 +3,6 @@ from collections import deque
 
 import numpy as np
 
-from sklearn.preprocessing import normalize
 from clustering import ClusteringAlgorithm
 from dataset import Dataset, DatasetLoader
 from sklearn import metrics
@@ -17,11 +16,18 @@ class KMeans(ClusteringAlgorithm):
                  num_inputs,
                  num_outputs,
                  X,
-                 Y):
-        self.centers = [np.random.random(num_inputs) for i in range(num_outputs)]
-        self.X = normalize(X)
+                 Y,
+                 num_trials = 100):
+        self.num_inputs = num_inputs
+        self.num_outputs = num_outputs
+        self.X = X
         self.Y = Y
+        self.num_trials = num_trials
         self.dataset_size = len(self.X)
+        self.generate_new_centers()
+
+    def generate_new_centers(self):
+        self.centers = [np.random.random(self.num_inputs) for i in range(self.num_outputs)]
 
     def predict_single(self, input_pattern):
         center_distances = []
@@ -42,37 +48,39 @@ class KMeans(ClusteringAlgorithm):
         return [self.predict_single(input_pattern) for input_pattern in input_patterns]
 
     def run(self, max_epochs=1000):
+        for trial in range(self.num_trials):
+            print("Starting trial %d" % trial)
+            for epoch in range(1, max_epochs):
+                assignments = [[] for center in self.centers]
+                # assign each input pattern to a cluster
+                for input_pattern in self.X:
+                    cluster = self.predict_single(input_pattern)
+                    assignments[cluster].append(input_pattern)
 
-        for epoch in range(1, max_epochs):
-            assignments = [[] for center in self.centers]
-            # assign each input pattern to a cluster
-            for input_pattern in self.X:
-                cluster = self.predict_single(input_pattern)
-                assignments[cluster].append(input_pattern)
+                has_diff = False    # whether or not there was a change in the clusters
 
-            has_diff = False    # whether or not there was a change in the clusters
+                # update the centers
+                for i in range(len(self.centers)):
+                    if len(assignments[i]) > 0: # only update if cluster has assignments
+                        new_center = np.mean(assignments[i], axis=0)
+                        if any(np.not_equal(new_center, self.centers[i])):
+                            has_diff = True
+                            self.centers[i] = new_center
 
-            # update the centers
-            for i in range(len(self.centers)):
-                if len(assignments[i]) > 0: # only update if cluster has assignments
-                    new_center = np.mean(assignments[i], axis=0)
-                    if any(np.not_equal(new_center, self.centers[i])):
-                        has_diff = True
-                        self.centers[i] = new_center
+                # print statistics every 10 iterations through the dataset
+                if epoch % 10 == 0:
+                    new_predictions = self.predict(self.X)
 
-            # print statistics every 10 iterations through the dataset
-            if epoch % 10 == 0:
-                new_predictions = self.predict(self.X)
+                    homogeneity = metrics.homogeneity_score(self.Y, new_predictions)
+                    completeness = metrics.completeness_score(self.Y, new_predictions)
+                    print("completeness=%f, homogeneity=%f" % (completeness, homogeneity))
 
-                homogeneity = metrics.homogeneity_score(self.Y, new_predictions)
-                completeness = metrics.completeness_score(self.Y, new_predictions)
-                print("completeness=%f, homogeneity=%f" % (completeness, homogeneity))
+                    if not has_diff:
+                        print("No change in the clusters, stopping.")
+                        self.generate_new_centers()
+                        break
 
-                if not has_diff and epoch > 100:
-                    print("No change in the clusters, stopping.")
-                    break
-
-            epoch += 1
+                epoch += 1
 
 
 ds = DatasetLoader.load("seeds")
