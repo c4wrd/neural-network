4bfe80abd5fedef2d29c527791a5cfa465fa21de
Cory Forward <cory@linux.com>
2017-12-10 19:10:50 -0700

    Updated PSO

--- clustering/pso/__init__.py
+++ clustering/pso/__init__.py
@@ -1,67 +1,97 @@
+from clustering import ClusteringAlgorithm
 from clustering.pso.particle import Particle
 from sklearn.preprocessing import normalize as norm
 from sklearn import datasets as sk_data
+from sklearn import metrics
+
+import numpy as np
 import matplotlib.pyplot as plot
 
-class PSO:
+class PSO(ClusteringAlgorithm):
 
-    def __init__(self, centers, features, factor1, factor2, w, particles, data):
-        self.data = data
-        self.centers = centers
-        self.features = features
-        self.factor1, self.factor2 = factor1,factor2
+    def __init__(self,
+                 num_features,
+                 num_centers,
+                 x,
+                 y,
+                 personal_factor=2,
+                 global_factor=2,
+                 w=0.5,
+                 particles=10):
+        self.X = x
+        self.Y = y
+        self.calculate_bounds()
+        self.centers = num_centers
+        self.features = num_features
+        self.factor1, self.factor2 = personal_factor, global_factor
         self.inertia = w
         self.particles = self.initialize_particles(particles)
         self.gbest = None
         self.gbest_fitness = None
         self.find_global_best()
 
+    def calculate_bounds(self):
+        self.max_bound = np.amax(np.absolute(self.X))
+        self.min_bound = -self.max_bound
+        print("Computed bounds of (%f, %f)" % (self.min_bound, self.max_bound))
+
     def initialize_particles(self,particles):
-        return [Particle(self.centers, self.features, self.factor1, self.factor2, self.inertia) for i in range(particles)]
+        return [Particle(self.centers, self.features, self.factor1, self.factor2, self.inertia, self.min_bound, self.max_bound) for i in range(particles)]
 
     def find_global_best(self):
         for particle in self.particles:
-            particle.calculate_fitness(self.data)
+            particle.calculate_fitness(self.X)
             fitness = particle.fitness
             if self.gbest is None or fitness > self.gbest_fitness:
                 self.gbest = particle.current_state
                 self.gbest_fitness = fitness
+                self.gbest_particle = particle
 
-    def run(self):
-        for particle in self.particles:
-            particle.update_velocity(self.gbest)
-            particle.update_current_state(-10,10)
-        self.find_global_best()
+    def run(self, max_epochs=1000):
+        for epoch in range(max_epochs):
+            for particle in self.particles:
+                particle.update_velocity(self.gbest)
+                particle.update_current_state(self.min_bound,self.max_bound)
+            self.find_global_best()
 
+            new_predictions = self.gbest_particle.predict(self.X)
 
-if __name__ == "__main__":
-    nclusters=4
+            if epoch % 10 == 0:
+                homogeneity = metrics.homogeneity_score(self.Y, new_predictions)
+                completeness = metrics.completeness_score(self.Y, new_predictions)
+                print("completeness=%f, homogeneity=%f, quantization_error=%f" % (completeness, homogeneity, self.gbest_fitness))
 
-    x,y = sk_data.make_blobs(100,2,nclusters)
-    test_data = [x,y]
-    g_factor = 1.49
-    p_factor = 1.49
-    pso = PSO(nclusters,2,p_factor,g_factor,0.72,7,test_data)
-
-    plot.ion()
-    plot.show()
-
-    cmap = plot.cm.get_cmap('brg', 10)
-
-    for i in range(100):
-        pso.run()
-        for i in range(len(pso.particles)):
-            for center in pso.particles[i].current_state:
-                plot.scatter(center[0],center[1], c=cmap(i), s=3)
-                plot.scatter([p[0] for p in x], [p[1] for p in x], color="blue")
-                g_best = pso.gbest
-                plot.scatter([p[0] for p in g_best], [p[1] for p in g_best], color="green")
-        plot.pause(0.001)
-        plot.gcf().clear()
-
-        print("Fitness: ",pso.gbest_fitness)
-    print("Best Fitness Achieved: ",pso.gbest_fitness)
-    print(pso.gbest)
+            yield epoch
+#
+# if __name__ == "__main__":
+#     nclusters=2
+#     n_features=2
+#     x,y = sk_data.make_blobs(100,n_features,nclusters, cluster_std=1, random_state=1)
+#     g_factor = 1.49
+#     p_factor = 1.49
+#     inertia=0.5
+#     n_particles=7
+#     pso = PSO(n_features,nclusters, p_factor,g_factor,inertia,n_particles, x, y)
+#
+#     plot.ion()
+#     plot.show()
+#
+#     cmap = plot.cm.get_cmap('brg', 10)
+#
+#     for epoch in pso.run():
+#        # pso.run()
+#         for i in range(len(pso.particles)):
+#             for center in pso.particles[i].current_state:
+#                 plot.scatter(center[0],center[1], c=cmap(i), s=3)
+#                 plot.scatter([p[0] for p in x], [p[1] for p in x], color="blue")
+#                 g_best = pso.gbest
+#                 plot.scatter([p[0] for p in g_best], [p[1] for p in g_best], color="green")
+#         plot.pause(0.001)
+#         plot.gcf().clear()
+#
+#        # print("Fitness: ",pso.gbest_fitness)
+#     print("Best Fitness Achieved: ",pso.gbest_fitness)
+#     print(pso.gbest)
 
 #for particle in pso.particles:
 #    print(particle)
--- clustering/pso/particle.py
+++ clustering/pso/particle.py
@@ -5,9 +5,9 @@ from sklearn.metrics import homogeneity_score as h_score, completeness_score as 
 
 class Particle:
 
-    def __init__(self, centers, features, f1, f2, w):
-        self.current_state = np.random.uniform(-1, 1, (centers, features))#[np.random.uniform(features) for i in range(centers)]#[[rand.random() for f in range(features)] for i in range(centers)]
-        self.velocity = np.random.uniform(-1, 1, (centers, features)) # [np.random.rand(features) for i in range(centers)] #[[0 for f in range(features)] for i in range(centers)]
+    def __init__(self, centers, features, f1, f2, w, min_bound, max_bound):
+        self.current_state = np.random.uniform(min_bound, max_bound, (centers, features))#[np.random.uniform(features) for i in range(centers)]#[[rand.random() for f in range(features)] for i in range(centers)]
+        self.velocity = np.random.uniform(0.1*min_bound, 0.1*max_bound, (centers, features)) # [np.random.rand(features) for i in range(centers)] #[[0 for f in range(features)] for i in range(centers)]
         self.personal_best = self.current_state
         self.factor1 = f1
         self.factor2 = f2
@@ -18,11 +18,11 @@ class Particle:
         return 'State: ' + str(self.current_state) + '\nVelocity: ' + str(self.velocity)
 
     def update_velocity(self, gbest):
-        rand1 = rand.random()
-        rand2 = rand.random()
+        c1 = np.random.uniform(0, self.factor1)
+        c2 = np.random.uniform(0, self.factor2)
 
         for i in range(len(self.velocity)):
-            self.velocity[i] = [self.inertia*v + self.factor1 * rand1 * (p-c) + self.factor2 * rand2 * (g-c)
+            self.velocity[i] = [(self.inertia*v) + (c1 * (p-c)) + (c2 * (g-c))
                               for v,p,g,c
                               in zip(self.velocity[i],self.personal_best[i],gbest[i],self.current_state[i])
                             ]
@@ -45,30 +45,40 @@ class Particle:
                 else:
                     curr[i] = curr[i] + vel[i]
 
+    def predict(self, dataset, with_distance=False):
+        new_assignments = []
+        for d in dataset:
+            center_distances = []
+            for index, center in enumerate(self.current_state):
+                # append the output of the neuron and the neuron weights
+                center_distances.append([index, self.euclidian_distance(center, d)])
+
+            # find max by output and save the neuron's index
+            min_center = min(center_distances, key=lambda item: item[1])
+            new_assignments.append((min_center[0], min_center[1]))
+        if with_distance:
+            return new_assignments
+        else:
+            return [assignment[0] for assignment in new_assignments]
+
     # data expected as [[[],[],[],...],[classes]]
     def calculate_fitness(self, dataset):
-        labels = [x for x in range(len(set(dataset[1])))]
-        data = dataset[0]
-        class_center_pairs = [[label,center] for label,center in zip(labels,self.current_state)]
-
-        new_assignments = []
-        for d in data:
-            for pair in class_center_pairs:
-                dist = None
-                curr_label = None
-                if dist is None or dist > self.euclidian_distance(d,pair[1]):
-                    dist = self.euclidian_distance(d,pair[1])
-                    curr_label = pair[0]
-            new_assignments.append((curr_label, dist))
+        new_assignments = self.predict(dataset, with_distance=True)
 
         new_fitness = 0
         # calculate quantization error
         for index, center in enumerate(self.current_state):
             # create a list of all dataset items assigned to this cluster
+            center_fitness = 0
             assignments = [assignment for assignment in new_assignments if assignment[0] == index]
             for assignment in assignments:
-                new_fitness += assignment[1] / len(assignments)
+                center_fitness += assignment[1]
+            if len(assignments) > 0:
+                new_fitness += center_fitness / len(assignments)
+
         new_fitness /= len(self.current_state)
+        # objective is to minimize the error
+        new_fitness = -new_fitness
 
         if self.fitness is None or self.fitness < new_fitness:
             self.fitness = new_fitness
